4a8fec8ea3c95c6e0ae039092374a17b4daace2c
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-03 15:25:47 +0300

    task 05

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,79 +1,66 @@
-$('document').ready(function () {
-  const api = 'http://' + window.location.hostname;
-
-  $.get(api + ':5001:/api/v1/status/', function (response) {
-    if (response.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+window.addEventListener('load', function () {
+  $.ajax('http://127.0.0.1:5001/api/v1/status/').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
     } else {
-      $('DIV#api_status').removeClass('available');
+      $('#api_status').removeClass('available');
     }
   });
 
   $.ajax({
-    url: api + ':5001/api/v1/places_search/',
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
     type: 'POST',
     data: '{}',
-    contentType: 'application/json',
     dataType: 'json',
+    contentType: 'application/json',
     success: appendPlaces
   });
 
-  let amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
+  const amenities = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
       delete amenities[$(this).attr('data-id')];
     }
-    if (Object.values(amenities).length === 0) {
-      $('.amenities H4').html('&nbsp;');
+
+    if (Object.keys(amenities).length === 0) {
+      $('div.amenities h4').html('&nbsp');
     } else {
-      $('.amenities H4').text(Object.values(amenities).join(', '));
+      $('.amenities h4').text(Object.values(amenities).join(', '));
     }
   });
 
-  $('BUTTON').click(function () {
+  $('button').click(function () {
     $.ajax({
-      url: api + ':5001/api/v1/places_search/',
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
       type: 'POST',
-      data: JSON.stringify({ 'amenities': Object.keys(amenities) }),
-      contentType: 'application/json',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
       dataType: 'json',
+      contentType: 'application/json',
       success: appendPlaces
     });
   });
-});
 
-function appendPlaces (data) {
-  $('SECTION.places').empty();
-  $('SECTION.places').append(data.map(place => {
-    return `<ARTICLE>
-              <DIV class="title">
-                <H2>${place.name}</H2>
-                  <DIV class="price_by_night">
-                    ${place.price_by_night}
-                  </DIV>
-                </DIV>
-                <DIV class="information">
-                  <DIV class="max_guest">
-                    <I class="fa fa-users fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.max_guest} Guests
-                  </DIV>
-                  <DIV class="number_rooms">
-                    <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.number_rooms} Bedrooms
-                  </DIV>
-                  <DIV class="number_bathrooms">
-                    <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
-                    </BR>
-                    ${place.number_bathrooms} Bathrooms
-                  </DIV>
-                </DIV>
-                <DIV class="description">
-                  ${place.description}
-                </DIV>
-              </ARTICLE>`;
-  }));
-}
+  function appendPlaces (data) {
+    $('.places').empty();
+    for (const place of data) {
+      const template = `
+        <article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guests</div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+      $('.places').append(template);
+    }
+  }
+});
